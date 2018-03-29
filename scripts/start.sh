a31c5e02ef3e98b282e6a48d36bf03dca599c4f1
Piotr Piskiewicz <piotr.piskiewicz@newride.tech>
2018-03-29 12:07:12 +0200

    aio initial commit

---
+++ scripts/start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Prevent config files from being filled to infinity by force of stop and restart the container
+lastlinephpconf="$(grep "." /usr/local/etc/php-fpm.conf | tail -1)"
+if [[ $lastlinephpconf == *"php_flag[display_errors]"* ]]; then
+ sed -i '$ d' /usr/local/etc/php-fpm.conf
+fi
+
+# Display PHP error's or not
+if [[ "$ERRORS" != "1" ]] ; then
+ echo php_flag[display_errors] = off >> /usr/local/etc/php-fpm.conf
+else
+ echo php_flag[display_errors] = on >> /usr/local/etc/php-fpm.conf
+fi
+
+sed -i "s/expose_php = On/expose_php = Off/g" /usr/local/etc/php-fpm.conf
+
+echo "log_errors = On" >> /usr/local/etc/php/conf.d/docker-vars.ini
+echo "error_log = /dev/stderr" >> /usr/local/etc/php/conf.d/docker-vars.ini
+
+if [ ! -z "$PUID" ]; then
+  if [ -z "$PGID" ]; then
+    PGID=${PUID}
+  fi
+  deluser nginx
+  addgroup -g ${PGID} nginx
+  adduser -D -S -h /var/cache/nginx -s /sbin/nologin -G nginx -u ${PUID} nginx
+else
+  if [ -z "$SKIP_CHOWN" ]; then
+    chown -Rf nginx.nginx /var/www/html
+  fi
+fi
+
+# Start supervisord and services
+exec /usr/bin/supervisord -n -c /etc/supervisord.conf
+
